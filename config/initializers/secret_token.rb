924ab55e18105f61f1f54cdc6a1fb14ed0d14c71
Joon Lee <seouri@gmail.com>
2011-03-23 21:03:29 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scigency::Application.config.secret_token = '460f1217fc7ac0ec808a2c095731b9bd01ffa279d656a04214680ca0eca02bee1d1aef3cd8b4c01f2c7b20e6dcc565d4f9ed3d0f86f03847ef0b7855c692327f'
